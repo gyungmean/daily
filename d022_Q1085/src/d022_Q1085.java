1b4d886c68aff5ca4b5d6fee5a714dce0063a1f2
gyungmean <yogurt2819@gmail.com>
2020-09-23 00:24:38 +0900

    day22

--- d022_Q1085/src/d022_Q1085.java
+++ d022_Q1085/src/d022_Q1085.java
@@ -1,9 +1,25 @@
-
+import java.util.Scanner;
 public class d022_Q1085 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int w = sc.nextInt();
+		int h = sc.nextInt();
+		
+		int d1, d2;
+		if(x >= w - x) {d1 = w - x;}
+		else {d1 = x;}
+		
+		if(y >= h - y) {d2 = h - y;}
+		else {d2 = y;}
+		
+		int rslt;
+		if(d1 >= d2) {rslt = d2;}
+		else {rslt = d1;}
+		System.out.println(rslt);
+		sc.close();
 
 	}
-
 }
