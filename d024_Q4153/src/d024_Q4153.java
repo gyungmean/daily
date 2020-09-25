f1e95d66fc88bf779bdedf6dd4a4ae802f48e7e3
gyungmean <yogurt2819@gmail.com>
2020-09-25 23:34:48 +0900

    day24

--- d024_Q4153/src/d024_Q4153.java
+++ d024_Q4153/src/d024_Q4153.java
@@ -1,9 +1,40 @@
-
+import java.util.Scanner;
 public class d024_Q4153 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		while(a != 0) {
+			int temp;
+			if(a > c) {
+				if(a > b) {
+					temp = a;
+					a = c;
+					c = temp;
+				}
+			}
+			if(b > c) {
+				if(b > a) {
+					temp = b;
+					b = c;
+					c = temp;
+				}
+			}
+			
+			if(a* a + b * b == c * c) {
+				System.out.println("right");
+			}
+			else {
+				System.out.println("wrong");
+			}
+			a = sc.nextInt();
+			b = sc.nextInt();
+			c = sc.nextInt();
+		}
+		sc.close();
 	}
 
 }
