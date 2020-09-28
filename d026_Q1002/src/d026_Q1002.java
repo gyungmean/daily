475eaa9086a38d86d0422f365aa33026edf47039
gyungmean <yogurt2819@gmail.com>
2020-09-28 23:53:41 +0900

    day26

--- d026_Q1002/src/d026_Q1002.java
+++ d026_Q1002/src/d026_Q1002.java
@@ -3,6 +3,17 @@ public class d026_Q1002 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int i = 0; i < T; i++) {
+			int x1 = sc.nextInt();
+			int y1 = sc.nextInt();
+			int r1 = sc.nextInt();
+			int x2 = sc.nextInt();
+			int y2 = sc.nextInt();
+			int r2 = sc.nextInt();
+			
+			
+		}
 		sc.close();
 
 	}
